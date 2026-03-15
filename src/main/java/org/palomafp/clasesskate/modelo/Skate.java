d6f388f0960b62a41e1d2734dd0d67ee2246694e
Sergio Rodríguez Nobrega <sergio.rodrigueznobrega@educa.madrid.org>
2026-03-15 14:01:52 +0100

    añadido comentarios a una clase

--- src/main/java/org/palomafp/clasesskate/modelo/Skate.java
+++ src/main/java/org/palomafp/clasesskate/modelo/Skate.java
@@ -1,40 +1,75 @@
 package org.palomafp.clasesskate.modelo;
+/**
+ * Representa un skate disponible en la escuela de skate.
+ * @author Sergio y Débora
+ * @version 1.0
+ */
 public class Skate {
+    //Identificador único del skate.
     private int codSkate;
+    //Tipo del Skate
     private String tipo;
+    //Sistema del skate
     private String sistema;
-
+    /**
+     * Constructor de la clase Skate.
+     * @param codSkate Identificador único del skate.
+     * @param tipo Tipo del Skate.
+     * @param sistema Sistema del skate.
+     */
     public Skate(int codSkate, String tipo, String sistema) {
         this.codSkate = codSkate;
         this.tipo = tipo;
         this.sistema = sistema;
     }
 
-    // Getters y Setters
+    /**
+     * Obtiene el código del skate.
+     * @return El código del skate.
+     */
     public int getCodSkate() {
         return codSkate;
     }
-
+    /**
+     * Establece el identificador del skate.
+     * @param codSkate El código del skate
+     */
     public void setCodSkate(int codSkate) {
         this.codSkate = codSkate;
     }
-
+    /**
+     * Obtiene el tipo del skate.
+     * @return el tipo del skate
+     */
     public String getTipo() {
         return tipo;
     }
-
+    /**
+     * Establece el tipo del skate.
+     * @param tipo tipo del skate
+     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
+    /**
+     * Obtiene el sistema del skate.
+     * @return el sistema del skate
+     */
     public String getSistema() {
         return sistema;
     }
-
+    /**
+     * Establece el sistema del skate.
+     * @param sistema sistema del skate
+     */
     public void setSistema(String sistema) {
         this.sistema = sistema;
     }
-
+    /**
+     * Devuelve el objeto Skate.
+     * @return El objeto Skate.
+     */
     @Override
     public String toString() {
         return "Skate{" +
